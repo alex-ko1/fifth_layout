e09e92c950c90fafc1125f883c57bed19eb4d710
Sasha <komik.forever@gmail.com>
2021-12-27 17:59:27 +0200

    27.12

--- js/script.js
+++ js/script.js
@@ -1,3 +1,63 @@
 $(document).ready(function() {
+    $('.head__slider').slick({
+        dots: true,
+        arrows: false,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+        //pauseOnFocus: true,
+        pauseOnHover: true,
 
+        touchTreshhold: 5,
+        touchMove: true,
+        waitForAnimate: false,
+        variableHeight: false,
+
+    });
+    $('.share__slider').slick({
+        dots: true,
+        arrows: false,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+        //pauseOnFocus: true,
+        pauseOnHover: true,
+
+        touchTreshhold: 5,
+        touchMove: true,
+        waitForAnimate: false,
+        variableHeight: false,
+
+    });
+    $('.features__slider').slick({
+        dots: true,
+        arrows: false,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        //easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        //fade: true,
+        //pauseOnFocus: true,
+
+
+        touchTreshhold: 5,
+        touchMove: true,
+        waitForAnimate: false,
+        //variableHeight: false,
+        //variableWidth: true,
+
+    });
 });
